bc2f20cc7685d0dabd7abcd5fa22070f6c8e5223
Le Duc Toan <toan.le@vn.devinition.com>
2020-12-17 17:24:57 +0700

    remote

--- nodejs/src/routes/remote.route.js
+++ nodejs/src/routes/remote.route.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import { isAuth } from '../middleware/auth.middleware'
+import remoteCtrl from '../controllers/remote.controllers'
 const router = express.Router()
 
+router.use(isAuth)
 
-router.get("/", (req, res) => {
-
+router.post("/", async (req, res) => {
+    let cmdString = req.body.cmdString.toString()
+    let cmdResult = await remoteCtrl.runCmdString(cmdString)
+    // let getDockerImages = await remoteCtrl.getDockerImages();
+    let result = cmdResult.data.split('\n'),
+        titleList = result[0].split(/(\s{2,})/).filter(item => !/(\s{2,})/.exec(item))
+    result.pop()
+    result.shift()
+    let data = result.map(element => element.split(/(\s{2,})/).filter(item => !/(\s{2,})/.exec(item))),
+        imageList = []
+    data.forEach(item => {
+        let image = {}
+        for (let index = 0; index < item.length; index++) {
+            const element = item[index];
+            image[titleList[index]] = element
+        }
+        imageList.push(image)
+    });
+    res.json(imageList)
 });
 
 
